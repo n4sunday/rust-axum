b6a23a51360b9804f655c36cdbca24fd22990eac
n4sunday <dev.nattapon.s@gmail.com>
2022-10-20 04:14:10 +0700

    :tada: initial project

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+
+mod types;
+use types::Person;
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new().allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/people", get(get_people))
+        .layer(cors);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn get_people() -> impl IntoResponse {
+    let people = vec![
+        Person {
+            name: String::from("Person A"),
+            age: 36,
+            favorite_food: Some(String::from("Pizza")),
+        },
+        Person {
+            name: String::from("Person B"),
+            age: 5,
+            favorite_food: Some(String::from("Broccoli")),
+        },
+    ];
+
+    (StatusCode::OK, Json(people))
+}
